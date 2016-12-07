9d443abfa31b1693fcf86a7256202027bc7028c0
Sasha <sasha361322@gmail.com>
2016-12-08 00:37:32 +0300

    commit

--- src/main.java
+++ src/main.java
@@ -1,21 +1,53 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 public class main {
-    public static void main(String[] args) {
-        Scanner in = null;
-        try {
-            in = new Scanner(new File("test.txt"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+
+    public static void worst(List<Integer> arr){
+        int max=0,length=0;
+        for (int i=0; i < arr.size(); i++){
+            for (int j=i; j < arr.size(); j++){
+                int tmp_max=0,tmp_length=0;
+                for (int k = i; k <= j; k++ ){
+                    tmp_max += arr.get(k);
+                    tmp_length++;
+                }
+                if ((tmp_max>max)||((tmp_max==max)&&(tmp_length<length))){
+                    max = tmp_max;
+                    length = tmp_length;
+                }
+            }
         }
-        List<Integer> arr = new LinkedList<Integer>();
-        while (in.hasNext()){
-            arr.add(in.nextInt());
+        if (max!=0)
+            System.out.println("max = "+max+" length = "+length);
+        else
+            System.out.println("Положительных чисел нет");
+    }
+
+    public static void middle(List<Integer> arr){
+        int max=0,length=0;
+        for (int i=0; i < arr.size(); i++){
+            int tmp_max=0,tmp_length=0;
+            for (int j=i; j < arr.size(); j++){
+                tmp_max += arr.get(j);
+                tmp_length++;
+                if ((tmp_max>max)||((tmp_max==max)&&(tmp_length<length))){
+                    max = tmp_max;
+                    length = tmp_length;
+                }
+            }
         }
-        int max=0, length=0, temp_max=0, temp_length=0;
+        if (max!=0)
+            System.out.println("max = "+max+" length = "+length);
+        else
+            System.out.println("Положительных чисел нет");
+    }
+
+    public static void best(List<Integer> arr){
+        int temp_max=0, temp_length=0, max=0,length=0;
         for (int current:arr){
             temp_max+=current;
             if (temp_max<=0){
@@ -29,7 +61,7 @@ public class main {
                 max=temp_max;
                 length = temp_length;
             }
-            else if ((temp_max == max)||(temp_length<length)){
+            else if ((temp_max == max)&&(temp_length<length)){
                 max=temp_max;
                 length = temp_length;
             }
@@ -38,5 +70,21 @@ public class main {
             System.out.println("max = "+max+" length = "+length);
         else
             System.out.println("Положительных чисел нет");
+    }
+
+    public static void main(String[] args) {
+        Scanner in = null;
+        try {
+            in = new Scanner(new File("test.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        List<Integer> arr = new ArrayList<Integer>();
+        while (in.hasNext()){
+            arr.add(in.nextInt());
+        }
+        worst(arr);
+        middle(arr);
+        best(arr);
     }
 }
